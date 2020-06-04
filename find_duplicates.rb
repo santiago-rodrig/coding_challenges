9e08f6b3c3d3f9a177888df72018dede4cbb7596
Santiago Rodriguez <santo1996.29@gmail.com>
2020-06-04 10:22:08 -0400

    explain the thought process behind find_duplicates

--- find_duplicates.rb
+++ find_duplicates.rb
@@ -1,12 +1,21 @@
+# finds the remainder after removing the duplicates between two collections
 def duplicates(arr1, arr2)
-  # write your code here
+  # arr1: Array with integers, duplicate of a subset of arr2
+  # arr2: Array with integers
+
+  # While arr1 has at least 1 element
   while arr1.size > 0
+    # in arr2 find the index of the element that matches the first element
+    # in arr1
     pos = arr2.find_index(arr1[0])
+    # if there is such an element, delete from the beginning of arr1 and
+    # delete that element in arr2 at the fetched position
     if pos
       arr1.shift
       arr2.delete_at(pos)
     end
   end
+  # return the remainder of elements in arr2 sorted in ascending order
   arr2.sort
 end
 
