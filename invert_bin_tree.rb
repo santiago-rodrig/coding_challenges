a059cb67ecf23e1401a91a05228fb97d735a6fc2
Santiago Rodriguez <santo1996.29@gmail.com>
2020-08-12 11:17:35 -0400

    challenge: invert binary tree

---
+++ invert_bin_tree.rb
@@ -0,0 +1,25 @@
+# challenge: https://leetcode.com/problems/invert-binary-tree/
+
+# Definition for a binary tree node.
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+  return unless root
+
+  root.left, root.right = root.right, root.left
+  invert_tree(root.left)
+  invert_tree(root.right)
+
+  root
+end
+
